be6b5fc9a6649d562ec290e3bdd8067ccce48fad
Mahmoud <your@email.com>
2025-08-21 21:54:52 +0300

    without role

--- Controllers/NewsController.cs
+++ Controllers/NewsController.cs
@@ -19,7 +19,7 @@ namespace SawirahMunicipalityWeb.Controllers
             _imageService = imageService;
         }
 
-        [Authorize(Roles = "Admin")]
+        //[Authorize(Roles = "Admin")]
         [HttpPost("create_news")]
         public async Task<IActionResult> CreateNews([FromForm] CreateNewsItemFormData request)
         {
